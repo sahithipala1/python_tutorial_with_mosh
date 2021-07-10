6505db5cefe68446a8522b52d2a60be9979e306a
sahithipala1 <43152542+sahithipala1@users.noreply.github.com>
2021-07-10 09:11:25 -0400

    Create example5.py

---
+++ example5.py
@@ -0,0 +1,11 @@
+print(5 ** 4)
+print ((10+3)* 8)
+
+# If else Statements
+temperature = 34
+if temperature > 30:
+    print("It's a hot day")
+    print("Drink water")
+elif temperature > 20:
+    print("It's a nice day")
+    
